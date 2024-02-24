748cdd24d29c19d5ec37a69a8db68e3952d32d0e
blackPack07 <143304088+blackPack07@users.noreply.github.com>
2024-02-24 16:37:45 +0000

    done 03

--- basics/comparisions.js
+++ basics/comparisions.js
@@ -1,3 +1,15 @@
 let a = 2 < 1;
 let b = 2 != 7;
-console.log(b);
+// console.log(b);
+// console.log("2" > 1);
+// console.log("03" == 3);
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// console.log(undefined > 0);
+
+console.log("2" == 2);
+console.log("2" === 2);     //=== checks the values are similar or not as well as thier similar data type
