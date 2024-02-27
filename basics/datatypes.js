9d8a99691cef57314a87851781796bbc148bc92d
blackPack07 <143304088+blackPack07@users.noreply.github.com>
2024-02-27 19:10:56 +0000

    lec 9

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+// Primittive data types in Js
+
+// 7 types - String, Number, Boolean, Null, Undefined, Symbol, BigInt
+const score = 100;
+const userLoggedIn = false;
+const userEmail = null;
+let name;
+// console.log(name);
+
+const id = Symbol('725');
+const id2 = Symbol('725');
+
+console.log(id,id2);
+console.log(id === id2)
+
+
+// Non-primitive or Reference data types
+
+// array, objects, functions
